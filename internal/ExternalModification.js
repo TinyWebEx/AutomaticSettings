1c6898fa075b83fb613bf45777df959f97f5a2d9
rugk <rugk+git@posteo.de>
2019-06-06 14:50:47 +0200

    Add first implementation of external modifications

---
+++ internal/ExternalModification.js
@@ -0,0 +1,39 @@
+/**
+ * Any APIs to allow external modiifications of the internal state of this module.
+ *
+ * @public
+ * @module AutomaticSettings
+ */
+
+import * as HtmlMod from "./HtmlModification.js";
+import * as DomModel from "./DomModel.js";
+import * as LoadAndSave from "./LoadAndSave.js";
+
+/**
+ * Set an option from the outside.
+ *
+ * This sets the visible option and saves it, so the state is consistent.
+ * It also always triggers any registered triggers.
+ *
+ * @public
+ * @param  {string} option option to change
+ * @param  {Object} optionValue the value to set
+ * @param  {Object} [optionGroup] the option group of the setting
+ * @returns {Promise}
+ */
+export async function setOption(option, optionValue, optionGroup) {
+    const elOption = DomModel.getElementFromOptionId(option);
+    optionGroup = optionGroup || DomModel.getOptionGroup(elOption);
+
+    if (optionGroup) {
+        await DomModel.getHtmlElementsOfOptionsGroup(optionGroup).map((elCurrentOption) => {
+            const currOption = HtmlMod.getOptionIdFromElement(elCurrentOption);
+            LoadAndSave.applyOption(currOption, optionGroup, elCurrentOption, { option: optionValue });
+        });
+    } else {
+        // apply HTML
+        await LoadAndSave.applyOption(option, optionGroup, elOption, { option: optionValue });
+        // now trihgger saving of modified values
+        LoadAndSave.saveOption({target: elOption});
+    }
+}
